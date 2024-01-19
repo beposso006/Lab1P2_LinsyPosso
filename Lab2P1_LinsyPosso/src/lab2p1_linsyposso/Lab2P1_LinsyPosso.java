1502a2292936b7dc82954288bfc0492744f1ec5f
beposso006 <belenposso2006@gmail.com>
2024-01-19 13:16:13 -0600

    pre commit 2

--- Lab2P1_LinsyPosso/src/lab2p1_linsyposso/Lab2P1_LinsyPosso.java
+++ Lab2P1_LinsyPosso/src/lab2p1_linsyposso/Lab2P1_LinsyPosso.java
@@ -14,7 +14,7 @@ public class Lab2P1_LinsyPosso {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
